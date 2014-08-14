7b3697c40a903309ccae14509e79631abdc73d16
Simeon Ackerman <amseon@web.de>
2014-08-14 14:27:55 +0200

    Update e-mail validation and pid-creation

--- js/hooks.js
+++ js/hooks.js
@@ -1,14 +1,15 @@
 // after model is parsed - init form handlers
 __initFormHandlers = function () {
 
-	// example: check mail for @
-	/*
-	rdform.find('input[name="foaf:mbox"]').change(function() {
-		if ( $(this).val().search(/\@/) == -1 ) {
-			alert("wondering - no '@' in your mail...?!");
+	// example: check valid e-mail on change
+	// this doesnt prevend submitting the form!
+	$(rdform).find('input[name="foaf:mbox"]').change(function() {
+		$("." + _ID_ + "-alert").html("");
+		var val = $(this).val();
+		if ( /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val) == false) {
+			RDForm.showAlert( "info", "Invalid E-mail Address, please try again.");
 		}
-	})
-	*/
+	});
 
 }
 
@@ -18,13 +19,18 @@ __afterDuplicateClass = function ( thisClass ) {
 	
 }
 
+// before creating result object from html formula
+_createResult = function() {
+
+}
+
 // before creating the class properties from input values
 __createResultClassProperty = function( propertyContainer ) {
 
+	// create the hidden pid field
 	if ( $(propertyContainer).children("input").attr("name") == "pid" ) {
 		$(propertyContainer).children("input").val( Math.floor( Math.random() * 10 ) );
 	}
-
 
 }
 
