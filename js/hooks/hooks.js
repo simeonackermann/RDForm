30b9d6828c2a6b003bc7b3ab41b1ba69c0e9c682
Simeon Ackerman <amseon@web.de>
2014-11-05 10:12:23 +0100

    New RDForm_Hooks class

--- js/hooks/hooks.js
+++ js/hooks/hooks.js
@@ -1,66 +1,91 @@
 /*
 RDForm Hooks-File - to hook in on certain points of application execution
+
+Variables:
+_this.rdform 	- The RDForm-class. Plublic functions can accessed like: _this.rdform.showAlert( "info", "...");
+_this.$elem 	- The form element
 */
+RDForm_Hooks.prototype = {
 
-// after model is parsed - init form handlers
-__initFormHandlers = function () {
+	// after model is parsed - init form handlers
+	__initFormHandlers : function () {
+		var _this = this;
+		// example: check valid e-mail on change
+		// this doesnt prevend submitting the form!
+		_this.$elem.find('input[name="foaf:mbox"]').change(function() {
+			$("." + _this.rdform._ID_ + "-alert").html("");
+			var val = $(this).val();
+			if ( /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val) == false) {
+				_this.rdform.showAlert( "info", "Invalid E-mail Address, please try again.");
+			}
+		});
 
-	// example: check valid e-mail on change
-	// this doesnt prevend submitting the form!
-	$(rdform).find('input[name="foaf:mbox"]').change(function() {
-		$("." + _ID_ + "-alert").html("");
-		var val = $(this).val();
-		if ( /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val) == false) {
-			RDForm.showAlert( "info", "Invalid E-mail Address, please try again.");
+	},
+
+	// after instert existing data into the form
+	__afterInsertData : function() {
+		var _this = this;
+	},
+
+	// after the addLiteral button was clicked
+	__afterAddLiteral : function ( thisLiteral ) {
+		var _this = this;
+	},
+
+	// after the duplicateLiteral button was clicked
+	__afterDuplicateLiteral : function ( thisLiteral ) {
+		var _this = this;
+	},
+
+	// after the addClass button was clicked
+	__afterAddClass : function ( thisResource ) {
+		var _this = this;
+	},
+
+	// after the duplicateClass button was clicked
+	__afterDuplicateClass : function ( thisClass ) {
+		var _this = this;
+	},
+
+	// after the duplicateExternalResource button was pressed
+	__afterDuplicateExternalResource : function ( thisResource ) {
+		var _this = this;
+	},
+
+	// validate form-input on change value or on submit the form
+	__userInputValidation : function ( property ) {
+		var _this = this;
+		// return false if property value is not valid
+	},
+
+
+	// before creating the result object from the html form
+	_createResult : function() {
+		var _this = this;
+	},
+
+	// before creating the class properties from input values
+	__createResultClassProperty : function( propertyContainer ) {
+		var _this = this;
+
+		// create the hidden pid field
+		if ( $(propertyContainer).children("input").attr("name") == "pid" ) {
+			$(propertyContainer).children("input").val( Math.floor( Math.random() * 10 ) );
 		}
-	});
 
-}
+	},
 
-// after instert existing data into the form
-__afterInsertData = function() {
-}
+	// before generating the class object from input values and properties
+	__createClass : function ( thisClass ) {
+		var _this = this;
+	},
+} // end of hooks
 
-// after the addLiteral button was clicked
-__afterAddLiteral = function ( thisLiteral ) {
-}
-
-// after the duplicateLiteral button was clicked
-__afterDuplicateLiteral = function ( thisLiteral ) {
-}
-
-// after the addClass button was clicked
-__afterAddClass = function ( thisResource ) {
-}
-
-// after the duplicateClass button was clicked
-__afterDuplicateClass = function ( thisClass ) {
-}
-
-// after the duplicateExternalResource button was pressed
-__afterDuplicateExternalResource = function ( thisResource ) {
-}
-
-// validate form-input on change value or on submit the form
-__userInputValidation = function ( property ) {
-	// return false if property value is not valid
-}
-
-
-// before creating the result object from the html form
-_createResult = function() {
-}
-
-// before creating the class properties from input values
-__createResultClassProperty = function( propertyContainer ) {
-
-	// create the hidden pid field
-	if ( $(propertyContainer).children("input").attr("name") == "pid" ) {
-		$(propertyContainer).children("input").val( Math.floor( Math.random() * 10 ) );
-	}
-
-}
-
-// before generating the class object from input values and properties
-__createClass = function ( thisClass ) {
+/*
+RDForm_Hooks class. Normally you dont need to edit this
+*/
+function RDForm_Hooks( rdform ) {
+	this.rdform = rdform;
+	this.$elem = rdform.$elem;
+	return this;
 }
